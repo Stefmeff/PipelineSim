608fcead0f9a3d9d5025a9d310cbe78e2195df06
Stefmeff <133770658+Stefmeff@users.noreply.github.com>
2025-04-13 20:16:22 +0200

    loadable

---
+++ Assets/Scripts/ComponentClasses/Loadable.cs
@@ -0,0 +1,50 @@
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * @author: Stefan Moser
+ *
+ * @brief: describes classes that can be saved as json files and reloaded into a respective gameObject
+ * */
+public abstract class Loadable
+{
+
+    //Position and Rotation for saving object to json file
+    [JsonProperty] public SerializableVector3 pos = new SerializableVector3(0, 0, 0);
+    [JsonProperty] public SerializableQuaternion rot = new SerializableQuaternion(0, 0, 0, 1);
+
+
+    /**
+    * @brief: Loads this components prefab and couples its monobehaviour with the components class.
+    **/
+    public abstract Component Load();
+
+    //Load and couple the pin classes of the component with the pin gameObjects
+    public abstract void LoadPins(InputPin_Mono[] inPins, OutputPin_Mono[] outPins);
+
+    //Load and couple gameObject that visualizes the delay
+    public abstract void LoadDelay(GameObject delayVisualizer);
+
+    /**
+     * Save the position and rotation of a game element for serialization
+     * */
+    public void SaveTransform(Transform t)
+    {
+        //save the position and rotation of this object so it can be serialized
+        Vector3 p = t.position;
+        Quaternion r = t.rotation;
+        this.pos = new SerializableVector3(p.x, p.y, p.z);
+        this.rot = new SerializableQuaternion(r.x, r.y, r.z, r.w);
+    }
+
+    //public abstract Component GetMainComponent();
+
+    public abstract void Reset();
+
+    public abstract void Dispose();
+
+    
+}
